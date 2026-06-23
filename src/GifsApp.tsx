ed0524fd3d1e6bb18c8014e3f384a648df9f72ae
Jesus David Limon Hernandez <jesus_limonhdz@hotmail.com>
2026-06-23 11:51:38 -0600

    Información temporal de los gifs

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,5 +1,41 @@
+import { mockGifs } from "./mock-data/gifs.mock"
+
 export const GifsApp = () => {
   return (
-    <div>GifsApp</div>
+    <>
+        <div className="content-center">
+            <h1>Buscador de Gifs</h1>
+            <p>Descubre y comparte el gif perfecto</p>
+        </div>
+
+        <div className="search-container">
+            <input type="text" placeholder="Buscar Gifs" />
+            <button>Buscar</button>
+        </div>
+
+        <div className="previous-searches">
+            <h2>Busquedas Previas</h2>
+            <ul className="previous-searches-list">
+                <li>Goku</li>
+                <li>Saitama</li>
+                <li>Denji</li>
+                <li>Elden ring</li>
+            </ul>
+        </div>
+
+        <div className="gifs-container">
+            {
+                mockGifs.map(gif => (
+                    <div key={gif.id} className="gif-card">
+                        <img src={gif.url} alt={gif.title} />
+                        <h3>{gif.title}</h3>
+                        <p>
+                            { gif.width} x { gif.height } (1.5mb)
+                        </p>
+                    </div>
+                ))
+            }
+        </div>
+    </>
   )
 }
